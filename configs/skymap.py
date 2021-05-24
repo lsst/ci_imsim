2077fa56dd31d52cd2fda21cc4addfbdb8e891a7
taranu <dtaranu@astro.princeton.edu>
2021-05-24 15:34:47 -0500

    Add ci_run_imsim.py and dependencies

---
+++ configs/skymap.py
@@ -0,0 +1,12 @@
+config.skyMap["discrete"].projection = "TAN"
+# center of tract 3828 patch 3,3
+config.skyMap["discrete"].raList = [56.64971166122064]
+config.skyMap["discrete"].decList = [-36.4462532139527947602893]
+config.skyMap["discrete"].radiusList = [1.4388796242707318]
+config.skyMap["discrete"].rotation = 0.0
+config.skyMap["discrete"].patchBorder = 100
+config.skyMap["discrete"].tractOverlap = 0.0
+config.skyMap["discrete"].pixelScale = 0.2
+config.skyMap["discrete"].patchInnerDimensions = [4000, 4000]
+config.skyMap.name = "discrete"
+config.name = 'discrete/ci_imsim'
